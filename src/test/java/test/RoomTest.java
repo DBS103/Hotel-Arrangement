b42e299a6fc9699ee70b0c3df4300b7508ad922b
railbai <1006452639@qq.com>
2020-01-11 16:36:43 +0800

    first8

---
+++ src/test/java/test/RoomTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.Test;
+
+import com.dbs.controller.Controllers;
+
+public class RoomTest {
+	
+	@Test
+	public void test01() {
+		Controllers c2 = new Controllers();
+		c2.selectRoom();
+	}
+
+}
