09220fa52248593a82785e15fa096b0b5c3d72c0
railbai <1006452639@qq.com>
2020-01-15 09:59:41 +0800

    customer 修改

--- src/main/java/com/dbs/controller/Controllers.java
+++ src/main/java/com/dbs/controller/Controllers.java
@@ -4,16 +4,32 @@ package com.dbs.controller;
 
 import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.dbs.pojo.Customer;
 import com.dbs.pojo.Room;
+
 import com.dbs.service.Services;
 import com.dbs.service.ServicesImpl;
 
+@Controller
 public class Controllers {
 	
 	private Services customerservice= new ServicesImpl();
 	
-	
+	@RequestMapping(value= {"/customer/**"})
+	public String customer() {
+		return "customerList";
+	}
+	@RequestMapping(value="/customersel/**")
+	@ResponseBody
+	public List<Customer> customersel(@RequestBody Customer customer) {
+		List<Customer> list = this.customerservice.selectCustomerService();
+		return list;
+	}
 	public void findCustomerByid(Integer clientno) {
 		Customer customer = this.customerservice.findCustomerByidService(clientno);
 		System.out.println(customer);
@@ -22,9 +38,9 @@ public class Controllers {
 	
 	public void addCustomer(Customer customer) {
 		if(this.customerservice.addCustomer(customer)) {
-			System.out.println("²åÈë³É¹¦");
+			System.out.println("ï¿½ï¿½ï¿½ï¿½É¹ï¿½");
 		}else {
-			System.out.println("²åÈëÊ§°Ü");
+			System.out.println("ï¿½ï¿½ï¿½ï¿½Ê§ï¿½ï¿½");
 		}
 	}
 	
@@ -39,9 +55,9 @@ public class Controllers {
 	public void updateCustomerByid(Customer customer) {
 		int num = this.customerservice.updateCustomerByidService(customer);
 		if(num>0) {
-			System.out.println("¸Äsuc");
+			System.out.println("suc");
 		}else {
-			System.out.println("Î´failed");
+			System.out.println("failed");
 		}
 	}
 	public void selectCustomer() {
--- src/main/java/com/dbs/service/EmpServiceImpl.java
+++ src/main/java/com/dbs/service/EmpServiceImpl.java
@@ -1,5 +1,6 @@
 package com.dbs.service;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
